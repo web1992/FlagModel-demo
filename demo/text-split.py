694ca91f17fd28244798350eaa878b037c6d3f1b
web1992 <554933654@qq.com>
2024-03-15 14:46:10 +0800

    ADD text-split

---
+++ demo/text-split.py
@@ -0,0 +1,25 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter, Language
+
+markdown_text = """
+# 🦜️🔗 LangChain
+
+⚡ Building applications with LLMs through composability ⚡
+
+## Quick Install
+
+```bash
+# Hopefully this code block isn't split
+pip install langchain
+```
+
+As an open-source project in a rapidly developing field, we are extremely open to contributions.
+"""
+
+
+md_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.MARKDOWN, chunk_size=1024, chunk_overlap=128
+)
+md_docs = md_splitter.create_documents([markdown_text])
+print(len(md_docs))
+for doc in md_docs:
+    print(doc)
