1ff1ffd2cd1fc7b8b0e6561ea4c410a9ea958acc
web1992 <554933654@qq.com>
2024-03-15 11:53:18 +0800

    ADD EmbeddingMode.py

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
+from flask import Flask, request, jsonify
 
-from flask import Flask
+from EmbeddingMode import ZGEmbedding
 
-# 创建一个Flask应用
-app = Flask(__name__)
 
-# 定义路由和视图函数
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+class App:
+    def __init__(self):
+        # 创建一个Flask应用
+        self.app = Flask(__name__)
+        self.embedding = initMode()
 
-# 如果直接运行这个文件，则启动Flask服务器
+        # 定义路由和视图函数
+        @self.app.route('/')
+        def hello():
+            return 'Hello, World!'
+
+        @self.app.route('/embedding')
+        def embedding():
+            doc = request.args.get('doc', '')
+            print('doc=', doc)
+            if len(doc) == 0:
+                embeddings_json = {
+                    'embeddingsList': []
+                }
+                # 返回JSON格式的embeddings_1
+                return jsonify(embeddings_json)
+            else:
+                embeddings_rs = self.embedding.getEmbeddingArray([doc])
+                embeddings_json = {
+                    'embeddingsList': embeddings_rs.tolist()
+                }
+                # 返回JSON格式的embeddings_1
+                return jsonify(embeddings_json)
+
+    def run(self):
+        # 启动Flask应用，监听在本地的5000端口
+        self.app.run(debug=True)
+
+
+def initMode():
+    embedding = ZGEmbedding('为这个句子生成表示以用于检索相关文章')
+    return embedding
+
+
 if __name__ == '__main__':
-    # 启动Flask应用，监听在本地的5000端口
-    app.run(debug=True)
+    # 创建并运行Flask应用
+    my_app = App()
+    my_app.run()
